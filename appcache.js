d0754279671f96e1c3a43a44f0f2b36c487f5853
digital labs <success@dpsw.info>
2013-01-20 16:21:37 -0600

    Update appcache.js

--- appcache.js
+++ appcache.js
@@ -1,17 +1,22 @@
 function appcache(options, app){
-    function setit(){
-        var lines = options.files, path = (options && options.path) || 'app.cache';
+    	options = options || {};
+    	var lines = (options && options.files) || [], 
+        	path = (options && options.path) || 'app.cache';
+
         lines.unshift('CACHE MANIFEST', '', 'CACHE:')
-        options.files.push('', '# ' + new Date())
+        lines.push('', '# ' + new Date())
+        var file = lines.join('\r\n')
+
         app.all(path, function(r, s){
             s.writeHead(200, {
                   'Content-Type': 'text/cache-manifest'
             })
-            s.end(options.files.join('\r\n'))
+            s.end(file)
         })
-    }  setit()
-    
-    if('reset' in options) setInterval(setit, options.reset)
+  		return file;
+
+    	if('reset' in options) setInterval(setit, options.reset)
 }
 
 if(module && module.parent && module.exports) module.exports = appcache;
+	else console.log(appcache({files:['file1', 'file2']}, {all:function(){}} ));
