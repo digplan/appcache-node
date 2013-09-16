e8bb68a37ffc77c83fe7954a14cb37fb45a4c137
digital labs <success@dpsw.info>
2013-09-15 20:08:21 -0500

    Update appcache.js

--- appcache.js
+++ appcache.js
@@ -1,8 +1,6 @@
 module.exports = {
     newCache: function(files){
-        files.unshift('CACHE MANIFEST', '', 'CACHE:');
-        files.push('', '# ' + new Date());
-        return files.join('\r\n');
+        return ['CACHE MANIFEST',,'CACHE:'].concat(files).concat([,'# '+Date()]).join('\r\n');
     },
     toDataURI: function(fn){
         return 'data:image/' + fn.match(/..\.(.*)$/)[1] + ';base64,' + 
