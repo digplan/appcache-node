7c08ddc5ba4100257029d6a652da5219e4cc3894
digital labs <success@dpsw.info>
2014-01-31 06:16:20 -0600

    formatting

--- appcache.js
+++ appcache.js
@@ -1,9 +1,8 @@
 module.exports = {
-    newCache: function(files){
-        return ['CACHE MANIFEST',,'CACHE:'].concat(files).concat([,'# '+Date()]).join('\r\n');
-    },
-    toDataURI: function(fn){
-        return 'data:image/' + fn.match(/..\.(.*)$/)[1] + ';base64,' + 
-            require('fs').readFileSync('./epic.png').toString('base64');
-    }
+  newCache: function(files) {
+    return ['CACHE MANIFEST', , 'CACHE:'].concat(files).concat([, '# ' + Date()]).join('\r\n');
+  },
+  toDataURI: function(fn) {
+    return 'data:image/' + fn.match(/..\.(.*)$/)[1] + ';base64,' + require('fs').readFileSync('./epic.png').toString('base64');
+  }
 }
